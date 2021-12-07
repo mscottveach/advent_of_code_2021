5892d78995150de4d8aaadbc1f66324c832dde63
m. scott veach <mscottveach@gmail.com>
2021-12-07 03:24:28 -0800

    update

--- day07.py
+++ day07.py
@@ -6,7 +6,6 @@ def avg(lst):
     return sum(lst) / len(lst)
 
 def align_crabs():
-
     with open('07-2-input.txt') as f:
         line = f.readline()
         fuel_cost = 0
@@ -16,13 +15,10 @@ def align_crabs():
         avg_ceil = math.ceil(avg_pos)
         avg_flor = math.floor(avg_pos)
         for crab in crab_positions:
-            n = abs(crab - avg_flor)
+            n = abs(crab - avg_ceil)
             fuel_cost += (n*(n+1))/2
 
         print(fuel_cost)
-
-
-
 
 if __name__ == '__main__':
     align_crabs()
